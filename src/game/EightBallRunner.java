925c253c7fc062cfef9cdff1ba9d92a3b1f93540
Tyler Price <tmp18298@uga.edu>
2025-11-02 20:45:55 -0500

    Update EightBallRunner.java

--- src/game/EightBallRunner.java
+++ src/game/EightBallRunner.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import game.EightBall;
 
 /**
- * Placeholder.
+ * Runs EightBall object.
  */
 public class EightBallRunner {
     public static void main (String[] args) {
